8d024241d09b3c0157c8ccca54c79e5ee25fd0db
Cameron Stuart <cameron@cameronstuart.com>
2019-08-05 10:29:10 -0600

    adds Setting Model

---
+++ Settings2/Settings2/Models/Setting.swift
@@ -0,0 +1,21 @@
+//
+//  Setting.swift
+//  Settings2
+//
+//  Created by Cameron Stuart on 8/5/19.
+//  Copyright © 2019 Cameron Stuart. All rights reserved.
+//
+
+import UIKit
+
+class Setting {
+    let name: String
+    let icon: UIImage
+    let isOn: Bool
+    
+    init(name: String, icon: UIImage, isOn: Bool = false) {
+        self.name = name
+        self.icon = icon
+        self.isOn = isOn
+    }
+}
